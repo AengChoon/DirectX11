20b19ecbd6e366fc3fd39309786b169316e63405
AengChoon <kevin1131@naver.com>
2024-06-29 11:22:08 +0900

    Update DirectX11.cpp

--- DirectX11.cpp
+++ DirectX11.cpp
@@ -2,83 +2,65 @@
 #include "framework.h"
 #include "DirectX11.h"
 
-#define MAX_LOADSTRING 100
+HINSTANCE GInstanceHandle;
+HWND GWindowHandle;
 
-// Global Variables:
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+ATOM MyRegisterClass(HINSTANCE InstanceHandle);
+BOOL InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-// Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+                      _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
-    // TODO: Place code here.
-
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_DIRECTX11, szWindowClass, MAX_LOADSTRING);
+    /** 1) 윈도우 창 정보 등록 */
     MyRegisterClass(hInstance);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
+    /** 2) 윈도우 창 생성 */
+    if (!InitInstance(hInstance, nCmdShow))
     {
         return FALSE;
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DIRECTX11));
-
-    MSG msg;
-
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    MSG Msg {};
+    while (Msg.message != WM_QUIT)
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        if (PeekMessage(&Msg, nullptr, 0, 0, PM_REMOVE))
         {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+	        TranslateMessage(&Msg);
+            DispatchMessage(&Msg);
+        }
+        else
+        {
+	        
         }
     }
 
-    return (int) msg.wParam;
+    return static_cast<int>(Msg.wParam);
 }
-
-
 
 //
 //  FUNCTION: MyRegisterClass()
 //
 //  PURPOSE: Registers the window class.
 //
-ATOM MyRegisterClass(HINSTANCE hInstance)
+ATOM MyRegisterClass(HINSTANCE InstanceHandle)
 {
-    WNDCLASSEXW wcex;
+    WNDCLASSEXW Wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+    Wcex.cbSize = sizeof(WNDCLASSEX);
+    Wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    Wcex.lpfnWndProc    = WndProc;
+    Wcex.cbClsExtra     = 0;
+    Wcex.cbWndExtra     = 0;
+    Wcex.hInstance      = InstanceHandle;
+    Wcex.hIcon          = LoadIcon(InstanceHandle, MAKEINTRESOURCE(IDI_DIRECTX11));
+    Wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    Wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW + 1);
+    Wcex.lpszMenuName   = nullptr;
+    Wcex.lpszClassName  = L"DirectX11";
+    Wcex.hIconSm        = LoadIcon(Wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DIRECTX11));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DIRECTX11);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
+    return RegisterClassExW(&Wcex);
 }
 
 //
@@ -93,20 +75,35 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+	GInstanceHandle = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+    RECT WindowRect {0, 0, GWindowSizeX, GWindowSizeY};
+    ::AdjustWindowRect(&WindowRect, WS_OVERLAPPEDWINDOW, false);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	GWindowHandle = CreateWindowW
+	(
+        L"DirectX11",
+        L"Client",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        0,
+        WindowRect.right - WindowRect.left,
+        WindowRect.bottom - WindowRect.top,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr
+    );
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	if (!GWindowHandle)
+    {
+		return FALSE;
+    }
 
-   return TRUE;
+	::ShowWindow(GWindowHandle, nCmdShow);
+	::UpdateWindow(GWindowHandle);
+
+	return TRUE;
 }
 
 //
@@ -129,9 +126,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             // Parse the menu selections:
             switch (wmId)
             {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
             case IDM_EXIT:
                 DestroyWindow(hWnd);
                 break;
@@ -155,24 +149,4 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
-}
-
-// Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
 }
